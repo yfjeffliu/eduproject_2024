e9e45b33f0a904f3ed67827561dcffc35314d036
劉彥甫 <37563959+yfjeffliu@users.noreply.github.com>
2024-05-22 17:09:10 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,7 +8,6 @@ while True:
         cpu_temp = f'{cpu.temperature:.2f}'
         group = '00'
         send_msg = f'GET /upload_data?group_name=group{group}&key=CPU_Temp&value={cpu_temp} HTTP/1.0'
-        # print(send_msg)
         Send_AT('AT+CIPSTART="TCP","140.116.179.11","8087"')
         time.sleep(1)
         Send_AT(f'AT+CIPSEND={len(send_msg)}')
